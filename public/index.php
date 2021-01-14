16ca16654b536ee50f62f3ae93bf37af0ef0af54
Roman <romanverzun2019@gmail.com>
2021-01-14 22:01:34 +0200

    Класс Router

--- public/index.php
+++ public/index.php
@@ -5,26 +5,37 @@
  */
 $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
+
+define('WWW', __DIR__ . '/../'); 
+define('CORE', dirname(__DIR__) . '/vendor/core');
+define('ROOT', dirname(__DIR__));
+define('APP', dirname(__DIR__) . '/app');
+
 /**
  * Подключение файлов и библиотек
  */
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
+//require '../app/controllers/Main.php';
+//require '../app/controllers/Posts.php';
+//require '../app/controllers/PostsNew.php';
+
+spl_autoload_register(function($class)
+{
+    $file = APP . "/controllers/$class.php";
+    if (is_file($file))
+    {
+        require_once $file;
+    }
+});
 
 $router = new Router();
 
 /**
  * Предопределенные маршруты
  */
-Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
-Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^pages/?(?P<action>[a-z-]+)?$', ['controller' => 'Posts']);
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 
-debug(Router::getRoutes());
-
-if (Router::matchRoute($query)) {
-    debug(Router::getRoute());
-} else
-{
-    echo "404";
-}
+Router::dispatch($query);  
